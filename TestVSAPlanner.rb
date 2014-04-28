c517ad208d15493dff964d9a56a13d617d0f7438
Robert Bruce Carleton <rbc@rbcarleton.com>
2014-04-27 21:21:30 -0700

    System 4 / Planner stub with test

---
+++ TestVSAPlanner.rb
@@ -0,0 +1,17 @@
+require "VSAPlanner"
+require "test/unit"
+
+class TestVSAPlanner < Test::Unit::TestCase
+    
+    def setup
+        @my_planner = VSAPlanner.new
+    end
+    
+    def teardown
+        ## Nothing really
+    end
+
+    def test_stub
+    end
+    
+end
---
+++ VSAPlanner.rb
@@ -0,0 +1,9 @@
+# A VSA planner class, system 4.
+#
+# -Responsible for looking outwards to the environment
+# -Communicates with Manager and Executive
+# -Determines system potentiality
+#
+class VSAPlanner
+
+end
